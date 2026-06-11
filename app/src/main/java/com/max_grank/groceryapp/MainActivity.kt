a8a48e503da69576d755e28b41f5d058f0e69e61
Maksim <maxgrank88@gmail.com>
2026-06-11 23:07:47 +0300

    created splash1_2

--- app/src/main/java/com/max_grank/groceryapp/MainActivity.kt
+++ app/src/main/java/com/max_grank/groceryapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.max_grank.groceryapp.splash_screen.Splash1_1
+import com.max_grank.groceryapp.splash_screen.Splash1_2
 import com.max_grank.groceryapp.ui.theme.GroceryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GroceryAppTheme {
-                Splash1_1()
+                Splash1_2()
             }
         }
     }
--- app/src/main/java/com/max_grank/groceryapp/splash_screen/Splash1_2.kt
+++ app/src/main/java/com/max_grank/groceryapp/splash_screen/Splash1_2.kt
@@ -9,79 +9,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import com.max_grank.groceryapp.R
 import com.max_grank.groceryapp.utils.NavigationButton
 import com.max_grank.groceryapp.utils.NavigationDot
 
-//Start 21:22
-//End 22:42
+//Start 22:56
+//Finish 23:07
 @Composable
 @Preview(showBackground = true)
-fun Splash1_1() {
+fun Splash1_2() {
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
         Box()
-         {
+        {
             Image(
                 painter = painterResource(
-                    R.drawable.maskgroup
+                    R.drawable.image_title
                 ),
-                contentDescription = "mask group",
+                contentDescription = "image_title",
                 modifier = Modifier
                     .fillMaxSize()
-            )
-        }
-        Box(
-            modifier = Modifier
-                .padding(top = 96.dp)
-                .align(Alignment.TopCenter)
-        ) {
-            Text(
-                text = "Welcome to",
-                fontSize = 30.sp,
-                fontWeight = FontWeight(700),
-                lineHeight = 1.3.em,
-                letterSpacing = 0.03.em,
-            )
-        }
-        Box(
-            modifier = Modifier
-                .padding(136.dp)
-        ) {
-            Image(
-                painter = painterResource(
-                    R.drawable.big_cart
-                ),
-                contentDescription = "big_cart",
-                modifier = Modifier
-                    .width(127.dp)
-                    .height(50.dp)
-            )
-        }
-        Box(
-            modifier = Modifier
-                .padding(top = 203.dp, start = 47.dp)
-        ) {
-            Image(
-                painter = painterResource(
-                    R.drawable.paragraph1
-                ),
-                contentDescription = "paragraph1",
-                modifier = Modifier
-                    .width(320.dp)
-                    .height(46.dp)
             )
         }
         Column(
@@ -93,11 +47,11 @@ fun Splash1_1() {
                     .padding(top = 700.dp, start = 166.dp)
             ) {
                 NavigationDot(
-                    isSelected = true
+                    isSelected = false
                 )
                 Spacer(modifier = Modifier.width(4.dp))
                 NavigationDot(
-                    isSelected = false
+                    isSelected = true
                 )
                 Spacer(modifier = Modifier.width(4.dp))
                 NavigationDot(
